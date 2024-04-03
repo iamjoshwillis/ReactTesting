796b31b64d307bac52105b659adecb44439f1359
Josh Willis <josh@infinitysoundfactory.com>
2024-04-03 13:00:37 -0400

    initialcommit

---
+++ src/Card.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Card from "./Card";
+
+//Smoke Test
+it("renders without crashing", () => {
+    render(<Card />);
+});
+
+
+//Snapshot Test
+it("matches snapshot", () => {
+    const { asFragment } = render(<Card />);
+    expect(asFragment()).toMatchSnapshot();
+})
+
+
